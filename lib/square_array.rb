cb82390e4152088f490110f9b75eb12e600d84a6
Ronda D Cowen <cowenhome5@gmail.com>
2019-08-15 18:17:24 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -9,8 +9,9 @@
 # end
 def square_array(array)
   counter=0 
+  new_array[]
   while array.length>counter do
-  array[counter]=new_array[]<<(array[counter]**2)
+  new_array<<(array[counter]**2)
 
    counter+=1 
   end
